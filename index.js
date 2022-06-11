dc656a79096193925b50de078b98ebb3780638a0
Mohammad Al Saleh <mo.alsaleh95@gmail.com>
2022-06-11 18:53:23 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded", () => {
+    printIt();
+});
+
+function printIt() {
+    document.querySelector("#text").innerHTML = "This is really cool!";
+
+}
